f009c7bfadc02ea5608d3803d4ce13f4b7de60b2
Jviry <johnvictoryaquino@gmail.com>
2026-04-15 16:23:04 +0800

    refactor: implement repository layer

---
+++ backend/repository/admin.repository.js
@@ -0,0 +1,37 @@
+import { hash } from "node:crypto";
+
+export function createAdminRepository(prisma) {
+  return {
+    async findByEmail(email) {
+      return prisma.admin.findUnique({
+        where: { email },
+      });
+    },
+
+    async create({ email, name, password }) {
+      return prisma.admin.create({
+        data: {
+          email,
+          name,
+          password,
+          role: 'admin',
+        }
+      });
+    },
+
+    async updatePassword(id, hashedPassword) {
+      return prisma.admin.update({
+        where: { admin_id: Number(id) },
+        data: { password: hashedPassword },
+      });
+    },
+
+    async delete(id) {
+      return prisma.admin.delete({
+        where: {
+          admin_id: Number(id),
+        },
+      })
+    },
+  }
+}
