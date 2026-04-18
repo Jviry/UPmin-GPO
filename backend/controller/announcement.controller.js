fdf19516ad576afda2a2a70511cb152f4d509014
Karlo Angelo O. Jose <kojose2@up.edu.ph>
2026-04-18 15:24:57 +0800

    feat: add route to apirouter

---
+++ backend/controller/announcement.controller.js
@@ -0,0 +1,6 @@
+import express from 'express';
+import { prisma } from '../db/db.js';
+
+const router = express.Router();
+
+export default router;
--- backend/controller/api.router.js
+++ backend/controller/api.router.js
@@ -1,6 +1,7 @@
 import adminRoute from './admin.controller.js';
-
+import announcementRoute from './announcement.controller.js';
 
 export const apiController = (app) => {
   app.use('/', adminRoute); //routers
+  app.use('/', announcementRoute); 
 };
