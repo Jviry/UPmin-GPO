31d03567e6996b4d19b294eac38d346178857c75
Jviry <johnvictoryaquino@gmail.com>
2026-05-04 22:16:07 +0800

    fix: validate create course

--- backend/usecase/course/createCourse.usecase.js
+++ backend/usecase/course/createCourse.usecase.js
@@ -1,7 +1,8 @@
+import { validateCreateCourseData } from '../../domain/course.js';
 export function createCourseUsecase(courseRepo) {
   return async function(courseData) {
     const { name, code, units, type } = courseData;
-    validateCreateCourse({ name, code, units, type });
+    validateCreateCourseData({ name, code, units, type });
     return await courseRepo.create({ name, code, units, type });
   };
 }
