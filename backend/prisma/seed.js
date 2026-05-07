1f6a85fe6278f2bd70150833cb96129d83b54216
Karlo Angelo O. Jose <kojose2@up.edu.ph>
2026-05-07 23:50:23 +0800

    feat: added initial program descriptions/history in db seed

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -67,12 +67,13 @@ async function main() {
   // =======================
   // PROGRAM + APPLICATION
   // =======================
-  const program = await prisma.program.create({
+
+  const dessProgram = await prisma.program.create({
     data: {
-      type: "Graduate Program",
-      name: "MS Computer Science",
-      description: "Advanced CS program",
-      history: "Program history...",
+      type: "Post Baccalaureate Program",
+      name: "Diploma in Exercise and Sports Science",
+      description: "The Diploma in Exercise and Sports Science (DESS) at UP Mindanao is a two-year, 39-unit post-baccalaureate program designed for professionals and non-related degree holders aiming to specialize in sports, fitness, and Physical Education. It bridges the gap for educators and practitioners, offering a formal academic foundation in human movement.",
+      history: "The Diploma in Exercise and Sports Science (DESS) program at UP Mindanao was established in school year 2017-2018.",
 
       program_application: {
         create: {
@@ -87,6 +88,131 @@ async function main() {
       program_application: true
     }
   });
+
+  const durpProgram = await prisma.program.create({
+    data: {
+      type: "Post Baccalaureate Program",
+      name: "Diploma in Urban and Regional Planning",
+      description: "The Diploma in Urban and Regional Planning (DURP) at UP Mindanao is a 26-unit postgraduate program designed to equip practitioners with the knowledge, skills, and attitude required for comprehensive development planning. It caters to planners, particularly in local government, to address growing urban development challenges.",
+      history: "The Diploma in Urban and Regional Planning (DURP) along with the Master in Urban and Regional Planning (MAURP) program at the University of the Philippines Mindanao was reoffered in the Second Semester of Academic Year 2016-2017. The program was revived under the Department of Architecture following approval by the UP Mindanao University Council in November 2016.",
+
+      program_application: {
+        create: {
+          qualifications: "BS CS or related",
+          application_instructions: "Submit docs",
+          application_url: "https://apply.com",
+          recommendation_url: "https://reco.com"
+        }
+      }
+    },
+    include: {
+      program_application: true
+    }
+  });
+
+  const maurpProgram = await prisma.program.create({
+    data: {
+      type: "Graduate Program",
+      name: "Master in Urban and Regional Planning",
+      description: "The Master of Arts in Urban and Regional Planning at UP Mindanao is a ladderized graduate program designed to develop leadership, management, and research skills for planning professionals, specifically addressing the growing demands of urbanization and development in the Mindanao region.",
+      history: "The Master in Urban and Regional Planning (MAURP) program at the University of the Philippines Mindanao was originally offered by the School of Management (SOM) before being revived under the Department of Architecture on November 2016.",
+
+      program_application: {
+        create: {
+          qualifications: "BS CS or related",
+          application_instructions: "Submit docs",
+          application_url: "https://apply.com",
+          recommendation_url: "https://reco.com"
+        }
+      }
+    },
+    include: {
+      program_application: true
+    }
+  });
+
+  const mshmsProgram = await prisma.program.create({
+    data: {
+      type: "Graduate Program",
+      name: "Master of Science in Human Movement Science",
+      description: "The Master of Science in Human Movement Science (MSHMS) at UP Mindanao is a 39-unit graduate program designed for advanced professional training in exercise science, physical education, and leisure studies. It focuses on scientific, research-oriented study to equip practitioners with skills in critical inquiry for careers in education, sports, and health.",
+      history: "The Master of Science in Human Movement Science (MSHMS) program was approved for adoption by the University of the Philippines Mindanao Board of Regents on April 3, 2023, with the program formally commencing in the 2023-2024 academic year.",
+
+      program_application: {
+        create: {
+          qualifications: "BS CS or related",
+          application_instructions: "Submit docs",
+          application_url: "https://apply.com",
+          recommendation_url: "https://reco.com"
+        }
+      }
+    },
+    include: {
+      program_application: true
+    }
+  });  
+
+  const msfsProgram = await prisma.program.create({
+    data: {
+      type: "Graduate Program",
+      name: "Master of Science in Food Science",
+      description: "The Master of Science in Food Science (MSFS) program at the University of the Philippines Mindanao is a graduate-level program under the Department of Food Science and Chemistry, designed to produce specialists in food safety, security, and innovation. It trains professionals to address food industry challenges in Mindanao, focusing on chemical, physical, and microbiological aspects of food processing.",
+      history: "The Master of Science in Food Science program at the University of the Philippines Mindanao was established as part of the institution’s graduate offerings, with the university itself founded on February 20, 1995. The program was developed to address the growing need for food science expertise in the Mindanao region, particularly in response to the challenges faced by the local food industry. It has since become a key component of UP Mindanao’s commitment to providing high-quality graduate education and contributing to regional development through research and innovation in food science.",
+
+      program_application: {
+        create: {
+          qualifications: "BS CS or related",
+          application_instructions: "Submit docs",
+          application_url: "https://apply.com",
+          recommendation_url: "https://reco.com"
+        }
+      }
+    },
+    include: {
+      program_application: true
+    }
+  });  
+
+  const mmProgram = await prisma.program.create({
+    data: {
+      type: "Graduate Program",
+      name: "Master in Management",
+      description: "The Master in Management (MM) program is designed to hone professionals in managerial and supervisory positions by providing them with key management science tools, strategic management concepts, and organization-relevant approaches.\n The program initially provides our students with several analytical and quantitative courses taught through case studies, team projects, and lectures before giving them the opportunity to apply this knowledge to real-world cases in Mindanao. To graduate, students are required to draw on the sum of their theoretical knowledge and practical experience to develop a strategic plan for their chosen organization.",
+      history: "Program history...",
+
+      program_application: {
+        create: {
+          qualifications: "BS CS or related",
+          application_instructions: "Submit docs",
+          application_url: "https://apply.com",
+          recommendation_url: "https://reco.com"
+        }
+      }
+    },
+    include: {
+      program_application: true
+    }
+  });  
+
+  const phdByResearchProgram = await prisma.program.create({
+    data: {
+      type: "Doctor of Philosophy Program (PhD)",
+      name: "PhD by Research",
+      description: "The PhD by Research program aims to produce graduates who have contributed to the body of knowledge in specific fields of study or to have provided innovative, theory-based, systematic, and practical solutions to the significant concerns of specific industries. Graduates will have advanced systematic knowledge and skills applied in a highly specialized or complex multidisciplinary field of professional work, research, and/or further study that require management expertise, innovation, and leadership. Graduates will demonstrate an in-depth understanding of theories and concepts necessary to advance learning and/or professional practice as well as to practice research skills providing a critical perspective of the real-world complex issues related to a specific field of study.\n The PhD by Research program aims to hone further the research skill of the candidate, acquired mostly through experience under the professional guidance of specialist(s) in his/her particular area of specialization. The program allows the candidate to earn the degree through submission, and the successful defense of a dissertation, without the usual academic course requirements. The program requires the candidate to devote almost his/her entire residency to research.",
+      history: "The PhD by Research program in the University of the Philippines Mindanao School of Management was established to provide advanced research training, with the campus itself having been created on February 20, 1995. While the campus is a relatively young constituent university, its focus on graduate studies and research has matured over its 31-year history.",
+      program_application: {
+        create: {
+          qualifications: "BS CS or related",
+          application_instructions: "Submit docs",
+          application_url: "https://apply.com",
+          recommendation_url: "https://reco.com"
+        }
+      }
+    },
+    include: {
+      program_application: true
+    }
+  });   
 
   // =======================
   // FACULTY + CREDENTIALS
